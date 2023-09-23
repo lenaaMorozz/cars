f3ee7200c86c18c4b6f6e8e28d695dd5bc814566
lenaaMorozz <lenaamorozz@gmail.com>
2023-09-23 14:24:48 +0500

    changed entity

--- src/main/java/com/example/car_catalog/entity/CarEntity.java
+++ src/main/java/com/example/car_catalog/entity/CarEntity.java
@@ -1,4 +1,25 @@
 package com.example.car_catalog.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class CarEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String licensePlate;
+    private String color;
+    private String brand;
+    private int manufacturingYear;
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
 }
