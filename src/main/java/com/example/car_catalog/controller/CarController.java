0d6274d68f83f2c4546c842a390dfe396e9432ea
lenaaMorozz <lenaamorozz@gmail.com>
2023-09-23 14:24:17 +0500

    added controllers

--- src/main/java/com/example/car_catalog/controller/CarController.java
+++ src/main/java/com/example/car_catalog/controller/CarController.java
@@ -1,15 +1,35 @@
 package com.example.car_catalog.controller;
 
+import com.example.car_catalog.api.request.CarRequest;
+import com.example.car_catalog.api.response.CarResponse;
+import com.example.car_catalog.api.response.CarResponseStatistics;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/car")
 public class CarController {
-    @GetMapping
-    public ResponseEntity getCar() {
-        return ResponseEntity.ok("fjjf");
+    @GetMapping("/cars")
+    public List<CarResponse> getCar() {
+        return new ArrayList<>();
     }
+
+    @PostMapping("/")
+    public ResponseEntity<String> addCar(@RequestBody CarRequest request) {
+        return ResponseEntity.ok("The car {} added");
+    }
+
+    @DeleteMapping("/cars/{id}")
+    public ResponseEntity<Long> deleteById(@PathVariable long id) {
+        return ResponseEntity.ok(id);
+    }
+
+    @GetMapping("/cars/statistics")
+    public CarResponseStatistics getStatistics() {
+        return new CarResponseStatistics(3, LocalDateTime.now(), LocalDateTime.now());
+    }
+
 }
